043eb771ab78d4090e315194767e2ccfc89036ec
小过 <www.2127617756@qq.com>
2024-01-19 07:43:50 +0800

    Alter file_name Version 3.0

--- useful_pyscript/num_alter_name.py
+++ useful_pyscript/num_alter_name.py
@@ -1,22 +1,18 @@
 import os
 import os.path
-import filetype
+import mimetypes
 
 address_dir = os.path.dirname(__file__)  # 拿到目录名
 address_content_old = os.listdir(address_dir)  # 拿到目录中的每个文件名
 address_dir_content_new = []  # 待修改的文件名
 for adr_file in address_content_old:  # 判断是否为需要更改的文件,并保存到列表中
     adr_whole = os.path.join(address_dir, adr_file)
-    if os.path.isdir(adr_whole) or adr_whole.endswith('.py'):
-        continue
-    if str(filetype.guess(adr_whole).mime).startswith('video'):
+    result = mimetypes.guess_type(adr_whole)[0]
+    if result and result.startswith('video'):
         address_dir_content_new.append(adr_whole)
-
 i = 1
-# 开始修改
+开始修改
 for file_name_old in address_dir_content_new:
     appendix = os.path.splitext(file_name_old)[1]  # 分隔出后缀名
     os.rename(file_name_old, os.path.join(address_dir, f'1（{i}）' + appendix))
     i += 1
-
-
